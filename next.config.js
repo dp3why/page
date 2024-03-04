bf4ec3732e6fdb23e467b514d049846c9bf0de99
dp3why <dp3why@gmail.com>
2024-03-04 00:37:22 -0800

    config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["github.com", "github.io"], // List of domains your images will be loaded from
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
